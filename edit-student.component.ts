2fb52be2cab2dc7787c6cfb283495eb67146006f
182826 <115691659+182826@users.noreply.github.com>
2023-04-28 13:46:00 +0530

    Add files via upload

---
+++ edit-student.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl} from '@angular/forms';
+import { StudentsService }from '../students.service';
+import { ActivatedRoute}from '@angular/router';
+
+@Component({
+  selector: 'app-edit-student',
+  templateUrl: './edit-student.component.html',
+  styleUrls: ['./edit-student.component.css']
+})
+export class EditStudentComponent implements OnInit{
+constructor(private student:StudentsService,private router:ActivatedRoute){ }
+
+editStudent=new FormGroup({
+  name: new FormControl( '' ),
+  email: new FormControl( ''  )
+
+} );
+message:boolean=false
+ngOnInit(): void {
+  //console.log(this.router.snapshot.params.id);
+  this.student.getStudentById(this.router.snapshot.params['id']).subscribe((result)=>{
+    console.log(result);
+  });
+
+}
+UpdateData(){
+  
+}
+removeMessage(){
+this.message=false;
+}
+}
